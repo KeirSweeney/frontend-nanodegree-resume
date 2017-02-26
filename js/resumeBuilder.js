205663b28d3c20f0e5f670ea67916cd6d35ba9b9
Keir Sweeney <keir.sweeney@cohaesus.co.uk>
2017-02-26 12:36:10 +0000

    Include map and Projects section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,19 +1,19 @@
 //Please note: I understand the differnt use of syntax between JS files and JSON files, given that my js object exists in here, I decided to use single 
-  // quotes on the value only.
+// quotes on the value only.
 
 var bio = {
-  name : 'Keir',
-  role : 'Full Stack Developer',
+  name: 'Keir',
+  role: 'Full Stack Developer',
   contacts: {
-    mobile : '07557005307',
-    email : 'KeirJSweeney@gmail.com',
-    github : 'https://github.com/KeirSweeney',
-    location : 'London'
+    mobile: '07557005307',
+    email: 'KeirJSweeney@gmail.com',
+    github: 'https://github.com/KeirSweeney',
+    location: 'London'
   },
-  welcomeMessage : 'Hey!',
-  skills : ['JS', 'HTML', 'CSS', 'C#'],
-  biopic : 'https://avatars3.githubusercontent.com/u/9768005?v=3&s=460',
-  display : function() {
+  welcomeMessage: 'Hey!',
+  skills: ['JS', 'HTML', 'CSS', 'C#'],
+  biopic: 'https://avatars3.githubusercontent.com/u/9768005?v=3&s=460',
+  display: function() {
     var formattedName = HTMLheaderName.replace('%data%', bio.name);
     var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
     var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
@@ -21,10 +21,10 @@ var bio = {
     var formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
     var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
     var formattedWelcome = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
-    var formattedSkills = function () { 
+    var formattedSkills = function() {
       var skillsArray = [];
       bio.skills.forEach(function(val) {
-          skillsArray.push(HTMLskills.replace('%data%', val));
+        skillsArray.push(HTMLskills.replace('%data%', val));
       });
       return skillsArray;
     };
@@ -37,41 +37,35 @@ var bio = {
     $('#header').append(formattedWelcome);
     $('#header').append(HTMLskillsStart);
     $('#skills').append(formattedSkills);
-    }
+  }
 };
 
 var education = {
-  schools: [
-    {
-      name: 'Kingston University',
-      location: 'Kingston, UK',
-      degree: 'BSc',
-      majors: ['Computer Science (Games Programming)'],
-      dates: '2012-2015'
-    },
-    {
-      name: 'Trinity Catholic HighSchool',
-      location: 'Woodford Green, UK',
-      degree: 'A Levels',
-      majors: ['Computing', ' Music', ' Maths', ' Physics'],
-      dates: '2004-2011'
-    }
-  ],
-  onlineCourses: [
-    {
-      title: 'Udactiy Frontend Nanodegree',
-      school: 'Udacity',
-      dates: '2017-Current',
-      url: 'https://www.udacity.com/'
-    },
-    {
-      title: 'Full Stack Web Development Certification',
-      school: 'Free Code Camp',
-      dates: '2016-Current',
-      url: 'https://www.freecodecamp.com'
-    }
-  ],
-  display : function() {
+  schools: [{
+    name: 'Kingston University',
+    location: 'Kingston, UK',
+    degree: 'BSc',
+    majors: ['Computer Science (Games Programming)'],
+    dates: '2012-2015'
+  }, {
+    name: 'Trinity Catholic HighSchool',
+    location: 'Woodford Green, UK',
+    degree: 'A Levels',
+    majors: ['Computing', ' Music', ' Maths', ' Physics'],
+    dates: '2004-2011'
+  }],
+  onlineCourses: [{
+    title: 'Udactiy Frontend Nanodegree',
+    school: 'Udacity',
+    dates: '2017-Current',
+    url: 'https://www.udacity.com/'
+  }, {
+    title: 'Full Stack Web Development Certification',
+    school: 'Free Code Camp',
+    dates: '2016-Current',
+    url: 'https://www.freecodecamp.com'
+  }],
+  display: function() {
     $('#education').append(HTMLschoolStart);
 
     for (school in education.schools) {
@@ -106,23 +100,20 @@ var education = {
 };
 
 var work = {
-  jobs: [
-    {
-      employer: 'Cohaesus',
-      title: 'Junior Web Developer',
-      location: 'Old Street, London',
-      dates: '2016-Current',
-      description: 'Full stack developer'
-    },
-    {
-      employer: 'The Dangerous Kitchen',
-      title: 'Game Developer',
-      location: 'Heathrow, London',
-      dates: '2015-2016',
-      description: 'Unity Game developer'
-    }
-  ],
-  display : function() {
+  jobs: [{
+    employer: 'Cohaesus',
+    title: 'Junior Web Developer',
+    location: 'Old Street, London',
+    dates: '2016-Current',
+    description: 'Full stack developer'
+  }, {
+    employer: 'The Dangerous Kitchen',
+    title: 'Game Developer',
+    location: 'Heathrow, London',
+    dates: '2015-2016',
+    description: 'Unity Game developer'
+  }],
+  display: function() {
     $('#workExperience').append(HTMLworkStart);
 
     for (job in work.jobs) {
@@ -142,18 +133,45 @@ var work = {
 };
 
 var projects = {
-  projects : [
-    {
-      title: 'Demo project', 
-      dates: '2017-current',
-      description: 'This is a demo project',
-      images: ['images/197x148.gif']
+  projects: [{
+    title: 'Demo project',
+    dates: '2017-current',
+    description: 'This is a demo project',
+    images: ['images/197x148.gif']
+  }],
+  display: function() {
+    $('#projects').append(HTMLprojectStart);
+
+    for (project in projects.projects) {
+      var formattedTitle = HTMLprojectTitle.replace('%data%', projects.projects[project].title);
+      var formattedDates = HTMLprojectDates.replace('%data%', projects.projects[project].dates);
+      var formattedDescription = HTMLprojectDescription.replace('%data%', projects.projects[project].description);
+      var imagesList = [];
+      var formattedImages = function() {
+        projects.projects[project].images.forEach(function(val) {
+          imagesList.push(HTMLprojectImage.replace('%data%', val));
+        })
+        return imagesList;
+      };
     }
-  ],
-  display : function() {
+
+
+    $('.project-entry').append(formattedTitle);
+    $('.project-entry').append(formattedDates);
+    $('.project-entry').append(formattedDescription);
+    $('.project-entry').append(formattedImages);
   }
 };
 
 bio.display();
+$('#mapDiv').append(googleMap);
 education.display();
+projects.display();
 work.display();
+
+var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
+var formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
+var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
+$('#footerContacts').append(formattedMobile, formattedEmail, formattedGithub, formattedLocation);
+
